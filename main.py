ba0f06fb43c7eb6563f2f26dc7fca6e419ddb6ec
Ali Nukhailawi <26713386+Ali-Haider8@users.noreply.github.com>
2026-01-03 02:08:16 +0300

    Initial commit

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("Hello from python")
+# print("Hello from python")
+# print("next line execute")
+
+
+def main():
+    print("Hello from python")
+print("next line execute")
+
+if __name__ == "__main__":main()
